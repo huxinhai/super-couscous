b76815028018cb6f26bd4fc439c151120fa07435
胡 <huxinhai807016@gmail.com>
2025-02-04 01:54:54 +0800

    add tab

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { createNativeStackNavigator as createStackNavigator } from '@react-navig
 import { Provider as MobxProvider } from 'mobx-react'
 import type React from 'react'
 import {
-  StatusBar,
+    StatusBar,
 } from 'react-native'
 import BootSplash from 'react-native-bootsplash'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
@@ -18,6 +18,7 @@ import Toast from 'react-native-toast-message'
  * @format
  */
 import Router from '@/router';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 const stores = {
 
@@ -27,44 +28,78 @@ const Stack = createStackNavigator<RootStackParamList>()
 enableFreeze(true)
 enableScreens()
 
+const Tab = createBottomTabNavigator()
+
+import Pay from "@/pages/Pay.tsx";
+
+const BottomTabNavigator = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarActiveTintColor: '#f39555',
+                tabBarInactiveTintColor: 'gray',
+                animation:'fade'
+            }}
+        >
+            <Tab.Screen
+                name={'Home'}
+                component={Pay}
+                options={{
+                    tabBarLabel: 'home'
+                }}
+            />
+
+        </Tab.Navigator>
+    )
+}
+
+
+
 function App(): React.JSX.Element {
 
-  return (
-    <GestureHandlerRootView>
-        <MobxProvider {...stores}>
-          <StatusBar
-            barStyle="dark-content"
-            backgroundColor="transparent"
-            translucent={!0}
-          />
-          <PaperProvider>
-            <Provider>
-                  <NavigationContainer
-                    onReady={() => BootSplash.hide({ fade: true })}
-                  >
-                    {/*<BottomTabNavigator/>*/}
-                    <Stack.Navigator
-                      screenOptions={
-                        {
-                          headerShown:!1,
-                        }
-                      }
-                    >
-                      {
-                        Router.map((item) =>
-                          <Stack.Screen
-                            key={item.name}
-                            {...item}
-                          />)
-                      }
-                    </Stack.Navigator>
-                  </NavigationContainer>
-            </Provider>
-          </PaperProvider>
-        </MobxProvider>
-        <Toast />
-    </GestureHandlerRootView>
-  )
+    return (
+        <GestureHandlerRootView>
+            <MobxProvider {...stores}>
+                <StatusBar
+                    barStyle="dark-content"
+                    backgroundColor="transparent"
+                    translucent={!0}
+                />
+                <PaperProvider>
+                    <Provider>
+                        <NavigationContainer
+                            onReady={() => BootSplash.hide({ fade: true })}
+                        >
+                            <Stack.Navigator
+                                // Main appear [RCTView setSheetLargestUndimmedDetent:]
+                                // pay appaer [RCTView setColor:]
+                                initialRouteName={'Main'}
+                                screenOptions={
+                                    {
+                                        headerShown:!1,
+                                    }
+                                }
+                            >
+                                <Stack.Screen
+                                    name="Main"
+                                    component={BottomTabNavigator}
+                                    options={{ headerShown: !1 }}
+                                />
+                                {
+                                    Router.map((item) =>
+                                        <Stack.Screen
+                                            key={item.name}
+                                            {...item}
+                                        />)
+                                }
+                            </Stack.Navigator>
+                        </NavigationContainer>
+                    </Provider>
+                </PaperProvider>
+            </MobxProvider>
+            <Toast />
+        </GestureHandlerRootView>
+    )
 }
 
 export default App
